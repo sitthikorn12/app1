433fc71bfbfa3e5325c04447d34dc16d4d7c4638
Sitthikorn <phraibueng12@gmail.com>
2025-12-25 12:02:04 +0700

    all component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { Header,Content,Footer  } from "./func-components";
+import { Header,Content,Footer  } from "./func-components"
+import Calendar from "./class-components";
 
 function App() {
   return (
     <>
     <Header/>
+    <p><center><Calendar/></center></p>
     <Content/>
-    <Footer/>
+    <Footer/> 
     </>
   ) 
 }
