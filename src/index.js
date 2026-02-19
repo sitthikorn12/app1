b5e86238710ab3208695a9d0a1e2a36fd8626811
Sitthikorn <phraibueng12@gmail.com>
2026-02-19 11:50:21 +0700

    TextArea

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import './index.css';
 import App from './App'
+import reportWebvitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
-const root = createRoot(document.getElementById('root'))
-root.render(<App />)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+    <React.StrictMode>
+    <App />
+    </React.StrictMode>
+);
+
+reportWebvitals();
